70c8bf7f3c092bb503828b1c8a9d9554011d3610
LinderJK <slepchenkovia@gmail.com>
2024-10-20 19:06:46 +0300

    feat: add create uuid to user model

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,3 +1,5 @@
+import createUUID from '../utils/uuidUtils'
+
 export interface User {
     name: string
     age: number
@@ -12,6 +14,8 @@ export const getAllUsers = () => users || 'No users found'
 export const getUser = (id: string) => users.find((user) => user.id === id)
 
 export const addUser = (user: User) => {
-    users.push(user)
+    const newUser = { ...user, id: createUUID() as string }
+
+    users.push(newUser)
     return user
 }
