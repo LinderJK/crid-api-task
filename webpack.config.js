770d0b1cf98132f06a0fd8ece67efcdb6bf7c7ec
LinderJK <slepchenkovia@gmail.com>
2024-10-20 17:51:21 +0300

    chore: change dependency configuration

--- webpack.config.js
+++ webpack.config.js
@@ -5,23 +5,23 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default {
-  entry: './src/index.ts',
-  target: 'node',
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    entry: './src/index.ts',
+    target: 'node',
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
 }
