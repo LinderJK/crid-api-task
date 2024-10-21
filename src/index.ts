a3936d9ae649c3384478e7aefe2acc0d9aaf44e6
LinderJK <slepchenkovia@gmail.com>
2024-10-21 12:22:13 +0000

    feat: add env config with dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+import dotenv from 'dotenv'
 import * as http from 'node:http'
 import userRoutes from './routes/userRoutes'
+
+const envFile =
+    process.env.NODE_ENV === 'production'
+        ? '.env.production'
+        : '.env.development'
+dotenv.config({ path: envFile })
 
 const PORT = process.env.PORT || 3000
 
