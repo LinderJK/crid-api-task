5627750fc34b2c46fc0472486f0c0d0051542154
LinderJK <slepchenkovia@gmail.com>
2024-10-21 19:07:38 +0300

    refactor: change env file name

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,9 @@ import * as http from 'node:http'
 import userRoutes from './routes/userRoutes'
 
 const envFile =
-    process.env.NODE_ENV === 'production'
-        ? '.env.production'
-        : '.env.development'
+    process.env.NODE_ENV === 'production' ? '.env.production' : '.env'
 dotenv.config({ path: envFile })
+
 const PORT = process.env.PORT || 3000
 
 const server = http.createServer(
