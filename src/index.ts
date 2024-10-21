0e91f49083a8ffe8ba32fefd169be8291ed61bb4
LinderJK <slepchenkovia@gmail.com>
2024-10-21 20:52:44 +0300

    feat: add create server function for tests

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,14 @@ dotenv.config({ path: envFile })
 const PORT = process.env.PORT || 3000
 const URL_LENGTH = 3
 
-const server = http.createServer(
-    (req: http.IncomingMessage, res: http.ServerResponse) => {
-        if (req.url === '/') {
-            res.writeHead(200, { 'Content-Type': 'text/html charset=utf-8' })
-            res.end(`<h1>Hello to CRUD API</h1>
+const createNewServer = () => {
+    return http.createServer(
+        (req: http.IncomingMessage, res: http.ServerResponse) => {
+            if (req.url === '/') {
+                res.writeHead(200, {
+                    'Content-Type': 'text/html charset=utf-8',
+                })
+                res.end(`<h1>Hello to CRUD API</h1>
                   <p>Main API route: /api/users</p>
                   <p>Methods: GET, POST, PUT, DELETE</p>
                   <p>Use /api/users to get all users</p>
@@ -21,30 +24,33 @@ const server = http.createServer(
                   <p>Methods PUT and DELETE require user id</p>
                   `)
 
-            return
+                return
+            }
+
+            const urlParts = req.url?.split('/') || []
+
+            if (
+                urlParts[1] === 'api' &&
+                urlParts[2] === 'users' &&
+                (urlParts.length === URL_LENGTH ||
+                    urlParts.length === URL_LENGTH + 1)
+            ) {
+                userRoutes(req, res)
+            } else {
+                res.statusCode = 404
+                res.end(
+                    JSON.stringify({
+                        message:
+                            'This api route does not exist please use a valid route',
+                    })
+                )
+            }
         }
-
-        const urlParts = req.url?.split('/') || []
-
-        if (
-            urlParts[1] === 'api' &&
-            urlParts[2] === 'users' &&
-            (urlParts.length === URL_LENGTH ||
-                urlParts.length === URL_LENGTH + 1)
-        ) {
-            userRoutes(req, res)
-        } else {
-            res.statusCode = 404
-            res.end(
-                JSON.stringify({
-                    message:
-                        'This api route does not exist please use a valid route',
-                })
-            )
-        }
-    }
-)
-
+    )
+}
+const server = createNewServer()
 server.listen(PORT, () => {
     console.log(`server started on port ${PORT}`)
 })
+
+export { server, createNewServer }
